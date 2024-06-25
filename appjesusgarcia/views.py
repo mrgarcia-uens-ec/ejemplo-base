be6f96c823a3f2c23bd20cd970870aef88a70bd7
mrgarcia-uens-ec <jesus.garcia@uens.edu.ec>
2024-06-25 17:23:56 +0000

    Clase 25-06-2024

--- appjesusgarcia/views.py
+++ appjesusgarcia/views.py
@@ -21,6 +21,8 @@ def lista_de_asignaturas(request):
     lista_asignaturas = Asignatura.objects.all()
     contexto = {
         "lista_asignaturas": lista_asignaturas,
+        "equipos_de_futbol": ["Real Madrid", "Atlético de Madrid", "Barcelona"],
+        "mi_nombre": "Jesús García"
     }
     return render(request, "asignaturas.html", contexto)
 
